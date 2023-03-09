c00721ced76744c2195f65dcb93c56aec3b53d5e
aa.medvedev <aa.medvedev@vk.team>
2023-03-09 15:32:18 +0400

    move useDownloadFiles to filesSlice

--- src/api/query/filesSlice.ts
+++ src/api/query/filesSlice.ts
@@ -1,19 +1,49 @@
-import type {
-    UploadFilesProps,
-    GetFilesProps,
-    GetFilesResponce,
-    UploadFilesResponce,
-} from '@/app/types';
+import type { UploadFilesProps, DownloadFilesProps, UploadFilesResponce } from '@/app/types';
 
 import { apiSlice } from './apiSlice';
 
 const filesSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
-        getFiles: builder.query<GetFilesResponce, GetFilesProps>({
-            query: ({ taskId, subTaskId, userId }) => ({
-                url: `/files/${taskId}`,
-                params: { subTaskId, userId },
-            }),
+        downloadFiles: builder.query<void, DownloadFilesProps>({
+            queryFn: async (
+                { taskId, subTaskId, userId },
+                _queryApi,
+                _extraOptions,
+                fetchWithBQ,
+            ) => {
+                const response = await fetchWithBQ({
+                    url: `/files/${taskId}`,
+                    responseHandler: (res) => res.blob(),
+                    params: { subTaskId, userId },
+                });
+
+                let fileName = '';
+                const disposition = response.meta?.response?.headers.get('Content-disposition');
+
+                if (disposition && disposition.indexOf('attachment') !== -1) {
+                    const filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                    const matches = filenameRegex.exec(disposition);
+
+                    if (matches != null && matches[1]) {
+                        fileName = matches[1].replace(/['"]/g, '');
+                    }
+                }
+
+                const result = {
+                    textblob: response.data as Blob,
+                    fileName: decodeURI(fileName.substring(5)),
+                };
+
+                let dwnlnk = document.createElement('a');
+                dwnlnk.download = result.fileName;
+
+                if (window.webkitURL != null) {
+                    dwnlnk.href = window.webkitURL.createObjectURL(result.textblob);
+                } else {
+                    dwnlnk.href = window.URL.createObjectURL(result.textblob);
+                }
+                dwnlnk.click();
+            },
         }),
         uploadFiles: builder.mutation<UploadFilesResponce, UploadFilesProps>({
             query: ({ taskId, subTaskId, files }) => ({
@@ -25,4 +55,4 @@ const filesSlice = apiSlice.injectEndpoints({
     }),
 });
 
-export const { useLazyGetFilesQuery, useUploadFilesMutation } = filesSlice;
+export const { useLazyDownloadFilesQuery, useUploadFilesMutation } = filesSlice;
