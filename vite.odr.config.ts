c83c68b6c7c21497a6d8024298b82c52b109dcfa
Sergey Minenkov <minenkov.sergey42@gmail.com>
2023-04-11 15:15:27 +0300

    [release-v.1.0.0] - update changelog and pachage.json

--- vite.odr.config.ts
+++ vite.odr.config.ts
@@ -1,11 +1,9 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ssl from '@vitejs/plugin-basic-ssl';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     base: './',
     build: {
