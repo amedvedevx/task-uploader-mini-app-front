517ea13378171972af8c9bd727cd4521394be8f3
Sergey Minenkov <s.minenkov@vk.team>
2023-06-06 14:11:24 +0300

    ME-42100, ME-42102, ME-42099, ME-42105, ME-42109: fix text overflow, fix tappable shadow, fix ui

--- src/pages/upload/components/UploadedFiles.tsx
+++ src/pages/upload/components/UploadedFiles.tsx
@@ -1,10 +1,10 @@
-import { Header } from '@vkontakte/vkui';
 import type { FC } from 'react';
 
 import { inclinationWord } from '@/lib';
 import type { TaskDetailResultContent } from '@/app/types';
 import { HorizontalScroll } from '@/components/HorizontalScroll';
 import { HorizontalFileCell } from '@/components/HorizontalFileCell';
+import { HeaderShort } from '@/components/HeaderShort';
 
 interface UploadedFilesProps {
     files: TaskDetailResultContent[];
@@ -12,10 +12,10 @@ interface UploadedFilesProps {
 
 export const UploadedFiles: FC<UploadedFilesProps> = ({ files }) => (
     <>
-        <Header mode='secondary'>
+        <HeaderShort mode='secondary'>
             {`Загружено ${files.length} 
                     ${inclinationWord(files.length, ['файл', 'файла', 'файлов'])}`}
-        </Header>
+        </HeaderShort>
 
         <HorizontalScroll data-automation-id='upload-page-filesList'>
             {files.map(({ title, docId }) => (
