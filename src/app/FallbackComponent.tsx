2110a227ef3a90f7bf0e5c8635db5b25d07dc7aa
Sergey Minenkov <minenkov.sergey42@gmail.com>
2023-06-14 17:14:40 +0300

    ME-42503: add changefragment event into error boundary

--- src/app/FallbackComponent.tsx
+++ src/app/FallbackComponent.tsx
@@ -1,6 +1,9 @@
 import type { FC } from 'react';
+import { useEffect } from 'react';
 import { AppRoot, Button } from '@vkontakte/vkui';
 import styled from 'styled-components';
+import type { VKBridgeEvent, ReceiveDataMap } from '@vkontakte/vk-bridge';
+import bridge from '@vkontakte/vk-bridge';
 
 import { Stub } from '@/components';
 import { AUTH_ERROR_MESSAGE } from '@/app/constants';
@@ -11,32 +14,49 @@ interface FallbackComponentProps {
     resetErrorBoundary: (...args: unknown[]) => void;
 }
 
-export const FallbackComponent: FC<FallbackComponentProps> = ({ error, resetErrorBoundary }) => (
-    <AppRoot>
-        <StubWrapper>
-            {error?.message === AUTH_ERROR_MESSAGE ? (
-                <StubAuth />
-            ) : (
-                <Stub
-                    title='Ошибка'
-                    subtitle={error?.message || 'Что-то пошло не так'}
-                >
-                    <br />
+export const FallbackComponent: FC<FallbackComponentProps> = ({ error, resetErrorBoundary }) => {
+    const bridgeEvents = ({ detail: { type } }: VKBridgeEvent<keyof ReceiveDataMap>) => {
+        if (type === 'VKWebAppChangeFragment') {
+            resetErrorBoundary();
+        }
+    };
 
-                    {resetErrorBoundary && (
-                        <Button
-                            stretched
-                            type='button'
-                            onClick={resetErrorBoundary}
-                        >
-                            Попробовать еще раз
-                        </Button>
-                    )}
-                </Stub>
-            )}
-        </StubWrapper>
-    </AppRoot>
-);
+    useEffect(() => {
+        bridge.subscribe(bridgeEvents);
+
+        return () => {
+            bridge.unsubscribe(bridgeEvents);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <AppRoot>
+            <StubWrapper>
+                {error?.message === AUTH_ERROR_MESSAGE ? (
+                    <StubAuth />
+                ) : (
+                    <Stub
+                        title='Ошибка'
+                        subtitle={error?.message || 'Что-то пошло не так'}
+                    >
+                        <br />
+
+                        {resetErrorBoundary && (
+                            <Button
+                                stretched
+                                type='button'
+                                onClick={resetErrorBoundary}
+                            >
+                                Попробовать еще раз
+                            </Button>
+                        )}
+                    </Stub>
+                )}
+            </StubWrapper>
+        </AppRoot>
+    );
+};
 
 const StubWrapper = styled.div`
     padding-top: 15vh;
