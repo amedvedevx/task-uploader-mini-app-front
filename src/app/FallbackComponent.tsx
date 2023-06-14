d614f95a766172d6e1094722ca0d54632c4fa357
Sergey Minenkov <minenkov.sergey42@gmail.com>
2023-06-14 17:58:54 +0300

    ME-42503:fix routing for error boundary

--- src/app/FallbackComponent.tsx
+++ src/app/FallbackComponent.tsx
@@ -2,12 +2,17 @@ import type { FC } from 'react';
 import { useEffect } from 'react';
 import { AppRoot, Button } from '@vkontakte/vkui';
 import styled from 'styled-components';
-import type { VKBridgeEvent, ReceiveDataMap } from '@vkontakte/vk-bridge';
+import type { VKBridgeEvent, ReceiveDataMap, ChangeFragmentResponse } from '@vkontakte/vk-bridge';
 import bridge from '@vkontakte/vk-bridge';
+import { useFirstPageCheck, useRouter } from '@happysanta/router';
 
 import { Stub } from '@/components';
 import { AUTH_ERROR_MESSAGE } from '@/app/constants';
 import { StubAuth } from '@/components/Stub/StubAuth';
+import { useGetPlatformQuery } from '@/api';
+import { checkIsMobilePlatform } from '@/lib';
+
+import { PAGE_UPLOAD_ID, PAGE_COLLECTION_ID, PAGE_COLLECTION_HOME } from './router';
 
 interface FallbackComponentProps {
     error: Error;
@@ -15,8 +20,26 @@ interface FallbackComponentProps {
 }
 
 export const FallbackComponent: FC<FallbackComponentProps> = ({ error, resetErrorBoundary }) => {
-    const bridgeEvents = ({ detail: { type } }: VKBridgeEvent<keyof ReceiveDataMap>) => {
+    const router = useRouter();
+
+    let isFirst = useFirstPageCheck();
+    const { data: platform = '' } = useGetPlatformQuery();
+    const isMobilePlatform = checkIsMobilePlatform(platform);
+
+    const bridgeEvents = ({ detail: { type, data } }: VKBridgeEvent<keyof ReceiveDataMap>) => {
         if (type === 'VKWebAppChangeFragment') {
+            const dataTyped = data as ChangeFragmentResponse;
+            const index = dataTyped.location?.lastIndexOf('/');
+            const id = dataTyped.location.substring(index + 1);
+
+            if (dataTyped.location.includes('upload')) {
+                router.pushPage(PAGE_UPLOAD_ID, { uploadId: id });
+                isFirst = true;
+            } else if (dataTyped.location.includes('collectionId')) {
+                router.pushPage(PAGE_COLLECTION_ID, { collectionId: id });
+            } else if (dataTyped.location === '') {
+                router.pushPage(PAGE_COLLECTION_HOME);
+            }
             resetErrorBoundary();
         }
     };
@@ -29,6 +52,12 @@ export const FallbackComponent: FC<FallbackComponentProps> = ({ error, resetErro
         };
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    useEffect(() => {
+        if (isMobilePlatform) {
+            bridge.send('VKWebAppSetSwipeSettings', { history: isFirst }).catch(() => {});
+        }
+    }, [isFirst, isMobilePlatform]);
 
     return (
         <AppRoot>
